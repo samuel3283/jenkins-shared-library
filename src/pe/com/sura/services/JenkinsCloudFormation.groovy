677e297d243d5945bffc988bf522425ba536d33a
Samuel Navarro <samuel.navarro_consultor@mdp.com.pe>
2021-10-28 21:35:07 -0500

    update library

--- src/pe/com/sura/services/JenkinsCloudFormation.groovy
+++ src/pe/com/sura/services/JenkinsCloudFormation.groovy
@@ -71,9 +71,9 @@ class JenkinsCloudFormation extends Base implements Serializable {
       passwordVariable: 'SECRET']]) {
         def dockerParameters = "--network=host"
         def dockerCommand
-		dockerCommand+=" aws configure set aws_access_key_id ${script.env.ACCESS} && aws configure set aws_secret_access_key ${script.env.SECRET} && aws configure set default.region ${script.env.AWS_REGION} && aws --version"       
-        dockerCommand+=" && aws cloudformation create-stack --stack-name stack002 --template-body file:///home/workspace/template.yml --parameters ParameterKey=ResourceName,ParameterValue=sura-dev-config-s3-demo ParameterKey=ParamTagProject,ParameterValue=PROYECTO001 ParameterKey=ParamTagEnv,ParameterValue=DEV "
-		String dockerCmd = "docker run ${dockerParameters} -v ${script.env.WORKSPACE}:/home/workspace ${script.env.REGISTRY_CONTAINER_URL}/${script.env.REGISTRY_ECR_NAME}:awscli-kubectl sh -c \"${dockerCommand}\""
+		dockerCommand+=" aws configure set aws_access_key_id ${script.env.ACCESS} && aws configure set aws_secret_access_key ${script.env.SECRET} && aws configure set default.region ${script.env.AWS_REGION} && aws --version"
+        //dockerCommand+=" && aws cloudformation create-stack --stack-name STACK_S3 --template-body template.yml --parameters ParameterKey=BucketNameParam,ParameterValue=sura-dev-configuraciones ParameterKey=ProyectoParam,ParameterValue=PROYECTO001 ParameterKey=AmbienteParam,ParameterValue=DEV "
+		String dockerCmd = "docker run ${dockerParameters} ${script.env.REGISTRY_CONTAINER_URL}/${script.env.REGISTRY_ECR_NAME}:awscli-kubectl sh -c \"${dockerCommand}\""
        
         
 
