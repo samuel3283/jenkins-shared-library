c51f8f8213d0d8ddd6d5c1181171e3c7905bbc0a
Samuel Navarro <samuel.navarro_consultor@mdp.com.pe>
2021-10-29 20:04:50 -0500

    update library

--- src/pe/com/sura/services/JenkinsCloudFormation.groovy
+++ src/pe/com/sura/services/JenkinsCloudFormation.groovy
@@ -114,7 +114,8 @@ class JenkinsCloudFormation extends Base implements Serializable {
 		def jsonResult = this.script.steps.sh(script: "cat parameter.json", returnStdout: true).trim()
 		JsonSlurper jsonSlurper = new JsonSlurper()
 		def jsonResultParsed = jsonSlurper.parseText(jsonResult.toString())
-
+		this.script.steps.echo "${jsonResult}"
+        this.script.steps.echo "${jsonResult['s3']}"
         
 /*
         String jsonResult = this.script.steps.sh (
