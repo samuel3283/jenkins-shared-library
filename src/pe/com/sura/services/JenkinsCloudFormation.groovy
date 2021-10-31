2553814e7ef2eec4c06dbc371f0a4318cb4b243d
Samuel Navarro <samuel.navarro_consultor@mdp.com.pe>
2021-10-30 19:08:26 -0500

    update library

--- src/pe/com/sura/services/JenkinsCloudFormation.groovy
+++ src/pe/com/sura/services/JenkinsCloudFormation.groovy
@@ -66,8 +66,8 @@ class JenkinsCloudFormation extends Base implements Serializable {
 	   
 	   def paramTag = getValuesTag()
 	   def paramS3 = getValuesS3()
-	   String fecha = this.buildTimestamp.toString()
-	   def nameStack = "stack-s3-${fecha}"
+	   //String fecha = this.buildTimestamp.toString()
+	   String nameStack = "stack-s3-${buildTimestamp}"
 	   
        dockerCommand+=" && aws cloudformation create-stack --stack-name ${nameStack} --template-body file:///home/workspace/template.yml --parameters ${paramS3} ${paramTag}"
 	   String dockerCmd = "docker run ${dockerParameters} ${dockerVolumen} ${script.env.REGISTRY_CONTAINER_URL}/${script.env.REGISTRY_ECR_NAME}:awscli-kubectl sh -c \"${dockerCommand}\""
